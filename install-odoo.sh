168d1d865f060463919212740023e26d62b4058b
newtratip <tharathipc@ecosoft.co.th>
2025-08-04 00:45:53 +0700

    /

--- install-odoo.sh
+++ install-odoo.sh
@@ -4,4 +4,5 @@ docker compose -f ./docker-compose.yml up -d
 chown -R $USER ./odoo
 docker exec -it -u 0 odoo chown -R odoo /var/lib/odoo
 cp ./odoo.conf ./odoo/config/odoo.conf
+docker exec -it -u 0 odoo apt-get install -y  xfonts-thai xfonts-thai-etl xfonts-thai-manop xfonts-thai-nectec xfonts-thai-poonlap xfonts-thai-vor
 docker restart odoo postgres
